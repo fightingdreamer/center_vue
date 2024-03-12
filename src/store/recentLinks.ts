c83cdd86e17a852155400924429b16ed02754f1c
Patryk Stępień <fighting.dreamer.0@gmail.com>
2024-03-12 02:19:27 +0100

    Save recentLinksStore state in localStorage

--- src/store/recentLinks.ts
+++ src/store/recentLinks.ts
@@ -6,8 +6,18 @@ export interface Link {
   location: string
 }
 
+function loadLinks(): Array<Link> {
+  const data = window.localStorage.getItem('recent-links') ?? '[]'
+  return JSON.parse(data)
+}
+
+function saveLinks(links: Array<Link>) {
+  const data = JSON.stringify(links)
+  window.localStorage.setItem('recent-links', data)
+}
+
 export const useRecentLinksStore = defineStore('recent-links', () => {
-  const links = shallowRef(new Array<Link>())
+  const links = shallowRef(loadLinks())
   const limit = shallowRef(3)
 
   function remember(name: string, location: string) {
@@ -15,6 +25,7 @@ export const useRecentLinksStore = defineStore('recent-links', () => {
       if (link.location == location) return
     }
     links.value = [{ name, location }, ...links.value].slice(0, limit.value)
+    saveLinks(links.value)
   }
 
   return { links, remember }
