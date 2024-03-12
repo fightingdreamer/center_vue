c26c08c59a448b4ca920624269d45fbd55d465b2
Patryk Stępień <fighting.dreamer.0@gmail.com>
2024-03-12 02:04:34 +0100

    Add reusable CopyToClipboard component

---
+++ src/components/reusable/CopyToClipboard.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import fileCopy from '@/assets/icons/file_copy.svg'
+import { copyToClipboard } from '@/lib/clipboard'
+
+defineProps<{
+  text: string
+}>()
+</script>
+
+<template>
+  <button @click="copyToClipboard(text)">
+    <img :src="fileCopy" alt="File copy" />
+  </button>
+</template>
